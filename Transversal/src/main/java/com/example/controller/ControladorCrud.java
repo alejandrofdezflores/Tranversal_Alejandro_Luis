6dbad059a19316ae179fff8c20110ad1fb1dd399
Alejandro Fernandez Flores <38208368+alejandrofdezflores@users.noreply.github.com>
2019-03-14 14:42:36 +0100

    Funcionando Thymeleaf

--- Transversal/src/main/java/com/example/controller/ControladorCrud.java
+++ Transversal/src/main/java/com/example/controller/ControladorCrud.java
@@ -12,81 +12,75 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
+ 
+import com.example.controller.*;
 import com.example.model.Alumno;
-
  
 @Controller
 @RequestMapping("/crud")
 public class ControladorCrud {
  
-    @Autowired
-    private AlumnoDao alumno;
+
+//    private UsuarioCrud uc;
  
-    // Mostrar todos los alumnos
-    @RequestMapping(value="", method = RequestMethod.GET)
+    @RequestMapping(path="", method = RequestMethod.GET)
     public String listaUsuarios(ModelMap mp){
-        mp.put("usuarios", alumno.getAllAlumnos());
+        mp.put("usuarios", AlumnoDao.getAllAlumnos());
         return "crud/lista";
     }
  
-    
-    @RequestMapping(value="/nuevo", method=RequestMethod.GET)
+    @RequestMapping(path="/nuevo", method=RequestMethod.GET)
     public String nuevo(ModelMap mp){
-        mp.put("alumno", new Alumno());
+        mp.put("usuario", new Alumno());
         return "crud/nuevo";
     }
  
-    @RequestMapping(value="/crear", method=RequestMethod.POST)
-    public String crear(@Valid Alumno a,
+    @RequestMapping(path="/crear", method=RequestMethod.POST)
+    public String crear(@Valid Alumno usuario,
             BindingResult bindingResult, ModelMap mp){
         if(bindingResult.hasErrors()){
             return "/crud/nuevo";
         }else{
-            alumno.saveAlumno(a);
-            mp.put("alumno", a);
+            AlumnoDao.saveAlumno(usuario);
+            mp.put("usuario", usuario);
             return "crud/creado";
         }
     }
- 
-    @RequestMapping(value="/creado", method = RequestMethod.POST)
-    public String creado(@RequestParam("alumno") Alumno a){
+ /*
+    @RequestMapping(path="/creado", method = RequestMethod.POST)
+    public String creado(@RequestParam("usuario") Alumno usuario){
         return "/crud/creado";
     }
     
-  /*  @RequestMapping(value="/borrar/{id}", method=RequestMethod.GET)
+    @RequestMapping(path="/borrar/{id}", method=RequestMethod.GET)
     public String borrar(@PathVariable("id") long id, ModelMap mp){
-    	Optional<Alumno> optionalUser = alumno.findById(id);
-    	Usuario user = optionalUser.get();
+    	Optional<Alumno> optionalUser = uc.findById(id);
+    	Alumno user = optionalUser.get();
         uc.delete(user);
         mp.put("usuarios", uc.findAll());
         return "crud/lista";
     }
-    */
-    /*
-    @RequestMapping(value="/editar/{id}", method=RequestMethod.GET)
+    
+    @RequestMapping(path="/editar/{id}", method=RequestMethod.GET)
     public String editar(@PathVariable("id") long id, ModelMap mp){
         mp.put("usuario", uc.findById(id));
         return "crud/editar";
     }
-     */
-    
-    /*
-    @RequestMapping(value="/actualizar", method=RequestMethod.POST)
+     
+    @RequestMapping(path="/actualizar", method=RequestMethod.POST)
     public String actualizar(@Valid Alumno usuario, BindingResult bindingResult, ModelMap mp){
         if(bindingResult.hasErrors()){
-            mp.put("alumno", alumno.getAllAlumnos());
+            mp.put("usuarios", uc.findAll());
         return "crud/lista";
         }
-        Optional<Alumno> optionalUser = alumno.getAllAlumnos(usuario.getId());
-        Alumno a = optionalUser.get();
+        Optional<Alumno> optionalUser = uc.findById(usuario.getId());
+        Alumno user = optionalUser.get();
         user.setNombre(usuario.getNombre());
-        user.setPassword(usuario.getPassword());
-        user.setEmail(usuario.getEmail());
+        user.setApellido(usuario.getApellido());
+        user.setId(usuario.getId());
         uc.save(user);
         mp.put("usuario", user);
         return "crud/actualizado";
-    }
-    */
+    }*/
  
 }
